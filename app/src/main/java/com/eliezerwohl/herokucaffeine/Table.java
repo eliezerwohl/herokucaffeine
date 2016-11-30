c0036fb676f41bdf973ee9d62983c4c2ed640339
Eliezer Wohl <eliezerwohl@gmail.com>
2016-11-30 16:05:53 -0500

    now adds info to the db! logs row count

---
+++ app/src/main/java/com/eliezerwohl/herokucaffeine/Table.java
@@ -0,0 +1,28 @@
+package com.eliezerwohl.herokucaffeine;
+
+import static android.R.attr.id;
+
+/**
+ * Created by Elie on 11/30/2016.
+ */
+
+public class Table {
+    private int id;
+    private String site;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+}
