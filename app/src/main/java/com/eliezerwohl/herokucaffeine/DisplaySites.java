c9b4046bddd9d1a7be586e68ea19443a74ce3d5b
Eliezer Wohl <eliezerwohl@gmail.com>
2016-12-01 16:25:32 -0500

    created custom adapter

---
+++ app/src/main/java/com/eliezerwohl/herokucaffeine/DisplaySites.java
@@ -0,0 +1,25 @@
+package com.eliezerwohl.herokucaffeine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class DisplaySites extends MainActivity {
+
+    @Override
+       protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_sites);
+        ListView lv = (ListView) findViewById(R.id.listView);
+         List testList = db.getAllSites();
+
+
+        ExtraAdapter adapter = new ExtraAdapter(this, R.layout.displayrow, testList);
+        lv.setAdapter(adapter);
+
+    }
+}
