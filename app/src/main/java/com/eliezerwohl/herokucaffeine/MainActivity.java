accdac9130d52d802523ef37184ada1821fc44be
Eliezer Wohl <eliezerwohl@gmail.com>
2016-12-08 15:43:27 -0500

    created notification whether program is running or not

--- app/src/main/java/com/eliezerwohl/herokucaffeine/MainActivity.java
+++ app/src/main/java/com/eliezerwohl/herokucaffeine/MainActivity.java
@@ -1,16 +1,21 @@
 package com.eliezerwohl.herokucaffeine;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import static android.R.string.no;
+import static android.os.Build.VERSION_CODES.N;
 import static com.eliezerwohl.herokucaffeine.R.id.jolt;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,11 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 currentStatus = "on";
                 currentStatusButton.setBackgroundColor(Color.GREEN);
             }
+
         }
         else{
             currentStatus = "off";
             currentStatusButton.setBackgroundColor(Color.RED);
         }
+        notifyUser(currentStatus);
         currentStatusButton.setText(currentStatus);
         currentStatusButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
@@ -55,14 +62,19 @@ public class MainActivity extends AppCompatActivity {
                     currentStatus = "on";
                     timerStart();
                     currentStatusButton.setBackgroundColor(Color.GREEN);
+
                 }
                 else{
                     currentStatus = "off";
                     timerStop();
                     currentStatusButton.setBackgroundColor(Color.RED);
+
                 }
+                notifyUser(currentStatus);
                 currentStatusButton.setText(currentStatus);
+
             }
+
         });
 
         jolt.setOnClickListener(new View.OnClickListener() {
@@ -85,6 +97,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(myIntent, 0);
             }
         });
+
+
     }
     public  void timerStart(){
         alarmManager=(AlarmManager) getSystemService(Context.ALARM_SERVICE);
@@ -108,6 +122,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void displaySites(View view){
         startActivity(new Intent(this, DisplaySites.class));
+    }
+    public void notifyUser(String string){
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.coffee)
+                        .setContentTitle("HEROKU CAFFEINE")
+                        .setContentText(string);
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(1, builder.build());
     }
 //    Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show();
 }
